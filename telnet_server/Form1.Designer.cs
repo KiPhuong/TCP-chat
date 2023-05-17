ea26aabf330c691bdf31b0c2c63fd5aad739ff4d
DinhBachKieuPhuong <21520406@ms.uit.edu.vn>
2023-05-17 14:30:10 +0700

    Add project files.

---
+++ telnet_server/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace telnet_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMess = new System.Windows.Forms.ListView();
+            this.btnNghe = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvMess
+            // 
+            this.lvMess.Location = new System.Drawing.Point(12, 48);
+            this.lvMess.Name = "lvMess";
+            this.lvMess.Size = new System.Drawing.Size(638, 255);
+            this.lvMess.TabIndex = 0;
+            this.lvMess.UseCompatibleStateImageBehavior = false;
+            this.lvMess.View = System.Windows.Forms.View.List;
+            // 
+            // btnNghe
+            // 
+            this.btnNghe.Location = new System.Drawing.Point(535, 12);
+            this.btnNghe.Name = "btnNghe";
+            this.btnNghe.Size = new System.Drawing.Size(115, 30);
+            this.btnNghe.TabIndex = 1;
+            this.btnNghe.Text = "Listen";
+            this.btnNghe.UseVisualStyleBackColor = true;
+            this.btnNghe.Click += new System.EventHandler(this.btnNghe_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(548, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(575, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnNghe);
+            this.Controls.Add(this.lvMess);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Bai05_Server_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListView lvMess;
+        private Button btnNghe;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ telnet_server/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace telnet_server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private bool isListening = false; // biến để đại diện cho trạng thái của nút lắng nghe
+        private Socket listenerSocket;
+        private IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("192.168.83.198"), 8080);
+        private List<Socket> ListClient;
+
+
+        private void btnNghe_Click(object sender, EventArgs e)
+        {
+            if (!isListening)
+            {
+                isListening = true;
+                btnNghe.Text = "Listening";
+                lvMess.Items.Add(new ListViewItem("Server chạy trên 192.168.83.198:8080. Đang lắng nghe kết nối...\r\n"));
+                StartUnsafeThread();
+            }
+            else
+            {
+                isListening = false;
+                btnNghe.Text = "Listen";
+                lvMess.Items.Add(new ListViewItem("Ngừng lắng nghe kết nối và đóng Server!\r\n"));
+                btnNghe.Enabled = false;
+                foreach (Socket socket in ListClient)
+                {
+                    socket.Send(Encoding.ASCII.GetBytes("Disconnected!"));
+                }
+                MessageBox.Show("Kết nối bị đóng bởi server!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listenerSocket.Close();
+            }
+
+        }
+
+        void StartUnsafeThread()
+        {
+            ListClient = new List<Socket>();
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenerSocket.Bind(ipepServer);
+            Thread listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        listenerSocket.Listen(100);
+                        Socket client1 = listenerSocket.Accept();
+                        ListClient.Add(client1);
+                        string str = "Client mới kết nối từ: " + client1.RemoteEndPoint.ToString() + "\n";
+                        lvMess.Items.Add(new ListViewItem(str));
+                        Thread recei = new Thread(receive);
+                        recei.IsBackground = true;
+                        recei.Start(client1);
+                    }
+                }
+                catch
+                {
+                    ipepServer = new IPEndPoint(IPAddress.Parse("192.168.101.45"), 8080);
+                    listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+            });
+            listen.IsBackground = true;
+            listen.Start();
+        }
+
+        void receive(object obj)
+        {
+            Socket client = obj as Socket;
+            Byte[] data = new byte[1024 * 5000];
+            while (true)
+            {
+                if (isListening)
+                {
+                    int receivedBytes = client.Receive(data);
+                    if (receivedBytes == 0)
+                    {
+                        // Kết nối đã đóng
+                        break;
+                    }
+                    string str = Encoding.ASCII.GetString(data, 0, receivedBytes);
+                    string content = client.RemoteEndPoint.ToString() + ": " + str + "\n";
+                    lvMess.Items.Add(new ListViewItem(content));
+
+                    Byte[] Sended = System.Text.Encoding.UTF8.GetBytes(content);
+                    int length_send = Sended.Length;
+
+                    foreach (Socket socket in ListClient)
+                    {
+                        if (socket != client)
+                        {
+                            socket.Send(Sended, length_send, SocketFlags.None);
+                        }
+                    }
+                    // Reset lại buffer
+                    Array.Clear(data, 0, data.Length);
+                }
+                else
+                {
+                    // Kết nối đã đóng
+                    break;
+                }
+            }
+        }
+
+
+        private void Bai05_Server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            foreach (Socket socket in ListClient)
+            {
+                socket.Send(UTF32Encoding.UTF32.GetBytes("Server ngừng kết nối!"));
+            }
+
+            if (isListening)
+                MessageBox.Show("Kết nối bị đóng bởi server!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            foreach (Socket socket in ListClient)
+            {
+                socket.Close();
+            }
+
+            listenerSocket.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mess = "KPhuong: " + textBox1.Text + "\n";
+            lvMess.Items.Add(new ListViewItem(mess));
+
+            Byte[] Sended = System.Text.Encoding.UTF8.GetBytes(mess);
+            int length_send = Sended.Length;
+
+            foreach (Socket socket in ListClient)
+            {
+                 socket.Send(Sended, length_send, SocketFlags.None);
+            }
+            textBox1.Text = string.Empty;
+        }
+    }
+}
